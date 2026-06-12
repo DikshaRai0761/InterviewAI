82968362df83e47a25937d081fbf8b5b97800186
DikshaRai0761 <diksharai07610761@gmail.com>
2026-06-12 15:52:59 +0530

    Fix production auth cookies

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -5,7 +5,7 @@ const tokenBlacklistModel = require("../models/blacklist.model")
 
 /**
  * @name registerUserController
- * @description register a new user, expects username, email and password in the request body
+ * @description register a new user
  * @access Public
  */
 async function registerUserController(req, res) {
@@ -19,7 +19,7 @@ async function registerUserController(req, res) {
     }
 
     const isUserAlreadyExists = await userModel.findOne({
-        $or: [ { username }, { email } ]
+        $or: [{ username }, { email }]
     })
 
     if (isUserAlreadyExists) {
@@ -42,8 +42,12 @@ async function registerUserController(req, res) {
         { expiresIn: "1d" }
     )
 
-    res.cookie("token", token)
-
+    res.cookie("token", token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+        maxAge: 24 * 60 * 60 * 1000
+    })
 
     res.status(201).json({
         message: "User registered successfully",
@@ -53,13 +57,11 @@ async function registerUserController(req, res) {
             email: user.email
         }
     })
-
 }
-
 
 /**
  * @name loginUserController
- * @description login a user, expects email and password in the request body
+ * @description login a user
  * @access Public
  */
 async function loginUserController(req, res) {
@@ -88,9 +90,15 @@ async function loginUserController(req, res) {
         { expiresIn: "1d" }
     )
 
-    res.cookie("token", token)
+    res.cookie("token", token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+        maxAge: 24 * 60 * 60 * 1000
+    })
+
     res.status(200).json({
-        message: "User loggedIn successfully.",
+        message: "User logged in successfully.",
         user: {
             id: user._id,
             username: user.username,
@@ -99,20 +107,24 @@ async function loginUserController(req, res) {
     })
 }
 
-
 /**
  * @name logoutUserController
- * @description clear token from user cookie and add the token in blacklist
- * @access public
+ * @description logout user
+ * @access Public
  */
 async function logoutUserController(req, res) {
+
     const token = req.cookies.token
 
     if (token) {
         await tokenBlacklistModel.create({ token })
     }
 
-    res.clearCookie("token")
+    res.clearCookie("token", {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none"
+    })
 
     res.status(200).json({
         message: "User logged out successfully"
@@ -121,14 +133,12 @@ async function logoutUserController(req, res) {
 
 /**
  * @name getMeController
- * @description get the current logged in user details.
- * @access private
+ * @description get current logged in user
+ * @access Private
  */
 async function getMeController(req, res) {
 
     const user = await userModel.findById(req.user.id)
-
-
 
     res.status(200).json({
         message: "User details fetched successfully",
@@ -138,10 +148,7 @@ async function getMeController(req, res) {
             email: user.email
         }
     })
-
 }
-
-
 
 module.exports = {
     registerUserController,
