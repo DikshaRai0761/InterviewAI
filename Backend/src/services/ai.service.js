780b67e7ad70df89f2ea8ec8952f972efa867376
DikshaRai0761 <diksharai07610761@gmail.com>
2026-06-12 23:35:09 +0530

    Fix PDF generation on Render

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -1,8 +1,8 @@
 const { GoogleGenAI } = require("@google/genai")
 const { z } = require("zod")
 const { zodToJsonSchema } = require("zod-to-json-schema")
+const chromium = require("@sparticuz/chromium")
 const puppeteer = require("puppeteer")
-
 const ai = new GoogleGenAI({
     apiKey: process.env.GOOGLE_GENAI_API_KEY
 })
@@ -58,17 +58,22 @@ async function generateInterviewReport({ resume, selfDescription, jobDescription
 
 
 async function generatePdfFromHtml(htmlContent) {
-    const browser = await puppeteer.launch()
-    const page = await browser.newPage();
-    await page.setContent(htmlContent, { waitUntil: "networkidle0" })
+
+    const browser = await puppeteer.launch({
+        args: chromium.args,
+        executablePath: await chromium.executablePath(),
+        headless: chromium.headless
+    })
+
+    const page = await browser.newPage()
+
+    await page.setContent(htmlContent, {
+        waitUntil: "networkidle0"
+    })
 
     const pdfBuffer = await page.pdf({
-        format: "A4", margin: {
-            top: "20mm",
-            bottom: "20mm",
-            left: "15mm",
-            right: "15mm"
-        }
+        format: "A4",
+        printBackground: true
     })
 
     await browser.close()
