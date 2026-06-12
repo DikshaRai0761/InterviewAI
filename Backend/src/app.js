cd74d4ee639c265ef0b351a71c2373d136320cdc
DikshaRai0761 <diksharai07610761@gmail.com>
2026-06-12 15:45:57 +0530

    Fix production CORS

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -10,16 +10,14 @@ app.use(cookieParser())
 app.use(cors({
     origin: [
         "http://localhost:5173",
-        "https://interview-ai-seven-teal.vercel.app"
+        "https://interview-ai-git-main-diksharai0761s-projects.vercel.app"
     ],
     credentials: true
 }))
 
-/* require all the routes here */
 const authRouter = require("./routes/auth.routes")
 const interviewRouter = require("./routes/interview.routes")
 
-/* using all the routes here */
 app.use("/api/auth", authRouter)
 app.use("/api/interview", interviewRouter)
 
