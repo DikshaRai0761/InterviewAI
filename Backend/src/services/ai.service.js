790dfe1e8c02bc7ff039afba77bb170ebb4bc22f
DikshaRai0761 <diksharai07610761@gmail.com>
2026-06-13 09:05:21 +0530

    Fix chromium version

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -1,8 +1,8 @@
 const { GoogleGenAI } = require("@google/genai")
 const { z } = require("zod")
 const { zodToJsonSchema } = require("zod-to-json-schema")
-const chromium = require("@sparticuz/chromium")
-const puppeteer = require("puppeteer")
+const chromium = require("@sparticuz/chromium");
+const puppeteer = require("puppeteer");
 const ai = new GoogleGenAI({
     apiKey: process.env.GOOGLE_GENAI_API_KEY
 })
@@ -58,27 +58,33 @@ async function generateInterviewReport({ resume, selfDescription, jobDescription
 
 
 async function generatePdfFromHtml(htmlContent) {
-
+    
     const browser = await puppeteer.launch({
         args: chromium.args,
         executablePath: await chromium.executablePath(),
-        headless: chromium.headless
-    })
+        headless: true,
+    });
 
-    const page = await browser.newPage()
+    const page = await browser.newPage();
 
     await page.setContent(htmlContent, {
-        waitUntil: "networkidle0"
-    })
+        waitUntil: "networkidle0",
+    });
 
     const pdfBuffer = await page.pdf({
         format: "A4",
-        printBackground: true
-    })
+        printBackground: true,
+        margin: {
+            top: "20mm",
+            bottom: "20mm",
+            left: "15mm",
+            right: "15mm",
+        },
+    });
 
-    await browser.close()
+    await browser.close();
 
-    return pdfBuffer
+    return pdfBuffer;
 }
 
 async function generateResumePdf({ resume, selfDescription, jobDescription }) {
